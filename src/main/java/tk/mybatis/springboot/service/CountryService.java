0fdc295abfe702c32c140cede829feef1cc862dd
isea533 <abel533@gmail.com>
2016-01-14 22:27:44 +0800

    更新通用Mapper和分页插件最新版本。

--- src/main/java/tk/mybatis/springboot/service/CountryService.java
+++ src/main/java/tk/mybatis/springboot/service/CountryService.java
@@ -44,7 +44,7 @@ public class CountryService {
 
     public List<Country> getAll(Country country) {
         if (country.getPage() != null && country.getRows() != null) {
-            PageHelper.startPage(country.getPage(), country.getRows());
+            PageHelper.startPage(country.getPage(), country.getRows(), "id");
         }
         return countryMapper.selectAll();
     }
