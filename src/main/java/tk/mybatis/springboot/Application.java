201879e0d16cbb8de9bd1693a0ead8b5dee89ad6
isea533 <abel533@gmail.com>
2017-09-06 20:52:02 +0800

    增加不同环境配置

--- src/main/java/tk/mybatis/springboot/Application.java
+++ src/main/java/tk/mybatis/springboot/Application.java
@@ -1,6 +1,9 @@
 package tk.mybatis.springboot;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -16,10 +19,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 @SpringBootApplication
 @MapperScan(basePackages = "tk.mybatis.springboot.mapper")
-public class Application extends WebMvcConfigurerAdapter {
+public class Application extends WebMvcConfigurerAdapter implements CommandLineRunner {
+    private Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("服务启动完成!");
     }
 
     @RequestMapping("/")
