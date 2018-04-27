e099ad1ca82b287652da033d58ac52c1bb3c2cd3
isea533 <abel533@gmail.com>
2018-04-28 06:44:02 +0800

    升级到最新版本

--- src/main/java/tk/mybatis/springboot/mapper/CountryMapper.java
+++ src/main/java/tk/mybatis/springboot/mapper/CountryMapper.java
@@ -24,9 +24,12 @@
 
 package tk.mybatis.springboot.mapper;
 
+import org.apache.ibatis.annotations.Select;
 import tk.mybatis.springboot.model.Country;
 import tk.mybatis.springboot.util.MyMapper;
 
 public interface CountryMapper extends MyMapper<Country> {
 
+    @Select("select * from country limit 1")
+    Country findOne();
 }
